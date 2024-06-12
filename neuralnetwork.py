37fb6b5d1d9e354272823d5bffa107a0cc36370d
PaulTradesnest <165902178+PaulTradesnest@users.noreply.github.com>
2024-06-12 14:35:55 +0100

    load and train data

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,2 +1,24 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O
+from matplotlib import pyplot as plt
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+data = pd.read_csv('/kaggle/input/digit-recognizer/train.csv')
+
+data.head()
+
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train =  data_train[0]
+X_train = data_train[1:n]
